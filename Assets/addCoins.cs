b20d5406d25583c46cd85c01ffc6cbbd3d0f93e8
21ABBMax <92145624+21ABBMax@users.noreply.github.com>
2022-12-16 12:46:37 +0100

    TEST

--- Assets/addCoins.cs
+++ Assets/addCoins.cs
@@ -10,7 +10,8 @@ public class addCoins : MonoBehaviour
         if(other.gameObject.tag == "Coins")
         {
             playerStats.amountOfCoins += 10;
-            Debug.Log("plus 10" + playerStats.amountOfCoins);
+           
         }
+        Debug.Log("plus 10" + playerStats.amountOfCoins);
     }
 }
